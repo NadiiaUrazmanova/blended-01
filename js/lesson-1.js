21c6be1f5be3a996c5ab12edc2c3d1ab8ad28369
Nadiia Urazmanova <nadegdaurazmanova@gmai.com>
2025-07-01 18:00:35 +0300

    solve tasks of blended-2

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -211,20 +211,20 @@
 
 
 
-function fizzBuzz(num) {
-    for (let i = 1; i <= num; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('fizzbuzz');
-        }
-        else if (i % 3 === 0) {
-            console.log('fizz');
-        }
-        else if (i % 5 === 0) {
-            console.log('buzz');
-        }
-        else {
-            console.log('none');
-        }
-    }
-}
-fizzBuzz(50);
+// function fizzBuzz(num) {
+//     for (let i = 1; i <= num; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('fizzbuzz');
+//         }
+//         else if (i % 3 === 0) {
+//             console.log('fizz');
+//         }
+//         else if (i % 5 === 0) {
+//             console.log('buzz');
+//         }
+//         else {
+//             console.log('none');
+//         }
+//     }
+// }
+// fizzBuzz(50);
